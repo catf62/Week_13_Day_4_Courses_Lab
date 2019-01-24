0fcb7e4043650e19dcc144398aec5216bfab635c
Catriona Ford <catriona.b.ford@gmail.com>
2019-01-24 10:25:44 +0000

    inital commit

---
+++ src/main/java/com/coursebooking/coursebooking/CoursebookingApplication.java
@@ -0,0 +1,14 @@
+package com.coursebooking.coursebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursebookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursebookingApplication.class, args);
+	}
+
+}
+
